672ed3d892aaccbeaaf0a923c250d4d835cef319
Gong Shiyun <gongshiyun@outlook.com>
2020-01-29 21:21:51 +0800

    question 8
    Signed-off-by: Gong Shiyun <gongshiyun@outlook.com>

---
+++ src/java/com/gsy/codinginterview/question7/Solution.java
@@ -0,0 +1,20 @@
+package com.gsy.codinginterview.question7;
+
+/**
+ * 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+ * n<=39
+ *
+ * @author gongshiyun
+ * @date 2020/1/29
+ */
+public class Solution {
+    public int Fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
